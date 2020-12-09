bb92ca50bba91fab7d95f3a217b75a656a9477da
Samuel Baetz <samuelbaetz105@gmail.com>
2020-12-08 23:45:08 -0500

    fixed syntax error

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Requiring necessary npm packages
 var express = require("express");
 var session = require("express-session");
-var app = express();
 var http = require('http').createServer(app)
 var io = require('socket.io')(http)
 var passport = require("./config/passport");
@@ -11,7 +10,7 @@ var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
-
+var app = require('express')();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -20,12 +19,17 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true 
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('connection success')
   socket.on('disconnect', () => {
-    console.log('disconnect success')
-    
-});
+      console.log('disconnect success')
+  })
+socket.on('message', (msg) => {
+    console.log('Message: ' + msg)
+    io.emit('message', msg)
+})
+})
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
