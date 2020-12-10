2fa29414360752abd3c2176c2976cf34e1ad12c8
Richard Walter <richardwalter515@gmail.com>
2020-12-10 18:57:52 -0500

    public js

---
+++ public/js/chat.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    // Getting references to our form and inputs
+    const searchButton = $("#searchUser");
+    let searchedName = $("input#name-input");
+  
+    // When the search button is clicked, we validate there's a name entered
+    searchButton.on("click", function(event) {
+      event.preventDefault();
+      var searchedData = {
+        name: searchedName.val().trim(),
+      };
+  
+      if (!searchedData.name) {
+        return;
+      }
+
+      // If we have a name, we run the populateMatchedUsers function and clear the form
+      populateMatchedUsers(searchedData.name);
+    });
+  
+    // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+    function populateMatchedUsers(name) {
+      //for each search result with that name, create an <ul> populating the list with <li> containing data from the database
+    }
+  });
