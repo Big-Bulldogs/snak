d20f94ea6efed2f2c1b245fd23b588051facca85
Samuel Baetz <samuelbaetz105@gmail.com>
2020-12-11 16:55:22 -0500

    handlebars sux

--- server.js
+++ server.js
@@ -1,50 +1,36 @@
-// Requiring necessary npm packages
-var express = require("express");
+var app = require('express')();
+var passport = require("./config/passport");
 var session = require("express-session");
 var http = require('http').createServer(app)
 var io = require('socket.io')(http)
-var passport = require("./config/passport");
-var exphbs = require("express-handlebars")
-// Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
+var express = require('express')
 var db = require("./models");
+var PORT = process.env.PORT || 8080;
 
-
-var app = require('express')();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(express.static('public'))
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+  });
 
-
-
-
-io.on('connection', (socket) => {
-  console.log('connection success')
-  socket.on('disconnect', () => {
-      console.log('disconnect success')
+  io.on('connection', (socket) => {
+      console.log('connection success')
+      socket.on('disconnect', () => {
+          console.log('disconnect success')
+      })
+    socket.on('message', (msg) => {
+        console.log('Message: ' + msg)
+        io.emit('message', msg)
+    })
   })
-socket.on('message', (msg) => {
-    console.log('Message: ' + msg)
-    io.emit('message', msg)
-})
-})
 
-
-
-require("./routes/api-routes.js")(app);
-app.get('/', function (req, res) {
-  res.render('profile');
-});
-// Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(
-  http.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-  }));
-  
+  require("./routes/api-routes.js")(app);
+  db.sequelize.sync().then(function() {
+  http.listen(PORT, () => {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT)
+  })
 });
