3d364f3eb210d91f481d8a6199dc766df4fec033
Samuel Baetz <samuelbaetz105@gmail.com>
2020-12-10 20:03:46 -0500

    handlebars issue

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ var exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+
 var app = require('express')();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-// We need to use sessions to keep track of our user's login status
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
