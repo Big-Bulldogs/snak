2a08cbe8f9c5d9f234e590808232acad40275ff3
Richard Walter <richardwalter515@gmail.com>
2020-12-12 14:18:52 -0500

    updated signup js with data to correspond with database

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   // Getting references to our form and input
   var signUpForm = $("form.signup");
   var emailInput = $("input#email-input");
+  var firstNameInput = $("input#firstname-input");
+  var lastNameInput = $("input#lastname-input");
+  var usernameInput = $("input#username-input");
   var passwordInput = $("input#password-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
@@ -9,23 +12,32 @@ $(document).ready(function() {
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
+      first_name: firstNameInput.val().trim(),
+      last_name: lastNameInput.val().trim(),
+      username: usernameInput.val().trim(),
       password: passwordInput.val().trim()
     };
 
-    if (!userData.email || !userData.password) {
+    if (!userData.email || !userData.password || !userData.first_name || !userData.last_name || !userData.username) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.email, userData.first_name, userData.last_name, userData.username, userData.password);
     emailInput.val("");
+    firstNameInput.val("");
+    lastNameInput.val("");
+    usernameInput.val("");
     passwordInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(email, first_name, last_name, username, password) {
     $.post("/api/signup", {
       email: email,
+      first_name: first_name,
+      lastt_name: last_name,
+      username: username,
       password: password
     })
       .then(function(data) {
