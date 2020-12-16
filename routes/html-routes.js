2e1b3d7a4798101e4e57ff232de422d1e314f68f
Samuel Baetz <samuelbaetz105@gmail.com>
2020-12-15 20:17:31 -0500

    fixing redirect

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the chat page
     if (req.user) {
-      res.redirect("/index");
+      res.redirect("/chat");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
@@ -32,7 +32,7 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/chat", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/chat.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
 };
